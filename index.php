380c091a5e2fa5c2ca7f60568dc9ce69edcc748e
matthieu.rodrigues <matthieu.rodrigues@epsi.fr>
2020-09-11 10:48:56 +0200

    Navbar + image profile dans Assets

--- index.php
+++ index.php
@@ -3,9 +3,46 @@
 <head>
   <meta charset="utf-8">
   <title>Microblogging</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+
   <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
+	<nav class="navbar navbar-light" style="background-color: #B3C7DD;">
+  		<div class="container-fluid">
+  			<div class="row w-100">
+  				<div class="col-md-2 offset-md-1">
+			  		<a class="navbar-brand" href="#">
+			    		<img src="/assets/logo.png" width="30" height="30" alt="" loading="lazy">
+			  		</a>
+		  		</div>
+
+		  		<div class="col-md-4 offset-md-1">
+					<form class="form-inline my-2 my-lg-0">
+				    	<input class="form-control w-75" type="search" placeholder="Search" aria-label="Search">
+				      	<button class="btn my-2 my-sm-0" type="submit">🔍</button>
+					</form>
+				</div>
+
+	  			<div class="col-md-2 offset-md-2">
+			  		<a class="navbar-brand" href="/profile">
+			    		<img src="/assets/profile.png" width="30" height="30" alt="" loading="lazy">
+			    		  <span class="navbar-text">
+						    Nom
+						    Prenom
+						  </span>
+						  <span class="navbar-text"></span>
+			  		</a>
+			  	</div>
+		  	</div>
+		</div>
+	</nav>
   Hello World
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>
