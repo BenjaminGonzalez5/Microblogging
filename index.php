c4994696e836e1a312f848fe7650907769cd1b21
BenjaminGonzalez5 <benjamin.gonzalez@epsi.fr>
2020-09-11 09:25:06 +0200

    Initialisation de la page d'accueil

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Microblogging</title>
+  <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+  Hello World
+</body>
+</html>
