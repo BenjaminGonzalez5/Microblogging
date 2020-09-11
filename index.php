4fc85387d9286d5d43f34bf850bc7896d8cafd46
BenjaminGonzalez5 <benjamin.gonzalez@epsi.fr>
2020-09-11 10:13:28 +0200

    Récupération des posts avec un début de design (à améliorer)

--- index.php
+++ index.php
@@ -11,17 +11,26 @@
   <meta charset="utf-8">
   <title>Microblogging</title>
   <link rel="stylesheet" href="assets/style.css">
-</head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	</head>
 <body>
 
-  <?php
+<?php
+	while ($donnees = $reponse->fetch()) {
+?>
 
-	  while ($donnees = $reponse->fetch()) {
-	  	echo $donnees['id'];
-	  	echo $donnees['content'];
-	  	echo $donnees['users_id'];
-	  	echo $donnees['date_post'];
-  ?>
+<div class="card w-50 mx-auto">
+  <div class="card-header">
+    <?php echo $donnees['date_post']; ?>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $donnees['users_id']; ?></h5>
+    <p class="card-text"><?php echo $donnees['content']; ?></p>
+  </div>
+</div>
 <br/>
 	<?php
 	  }
